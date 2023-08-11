19ff396c76e6cdaf44c6c05437151875b1f310f6
mumbuasuzanne <mumuasuzanne11@gmail.com>
2023-08-11 12:30:33 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,24 +3,24 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints the alphabet in lowercase, excluding the letters 'q' and 'e'.
- *              It only uses the putchar function and all the code is in the main function.
+ * Description: This program prints the alphabet in lowercase,
+ *              excluding the letters 'q' and 'e', using the putchar function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+    char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'q' && letter != 'e')
-		{
-			putchar(letter);
-		}
-	}
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e')
+        {
+            putchar(letter);
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return 0;
+    return 0;
 }
