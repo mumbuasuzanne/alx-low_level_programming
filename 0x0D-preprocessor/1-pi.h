cd607606a469cdaa3f9ac157c38aa6e53cfc4604
mumbuasuzanne <mumuasuzanne11@gmail.com>
2023-09-11 05:05:41 -0700

    1. Pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef MYMACROS_H
+#define MYMACROS_H
+
+/**
+ * File: mymacros.h
+ * Author: Your Name
+ * Desc: Header file defining macros.
+ */
+
+#define PI 3.14159265359
+
+#endif /* MYMACROS_H */
+
