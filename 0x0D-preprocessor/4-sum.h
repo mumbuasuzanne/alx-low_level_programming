3427fa852b16cf2af4c51c105276e975b817e460
mumbuasuzanne <mumuasuzanne11@gmail.com>
2023-09-11 05:19:06 -0700

    4. SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+/**
+ * SUM - Compute the sum of two numbers.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Description: This macro computes the sum of two given numbers 'x' and 'y'.
+ *
+ * Return: The sum of 'x' and 'y'.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_MACRO_H */
+
