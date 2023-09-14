f4a10f607a28790a791391f9936eed37d3c86978
mumbuasuzanne <mumuasuzanne11@gmail.com>
2023-09-14 14:38:51 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,103 @@
-include "variadic_functions.h"
+#include "variadic_functions.h"
+ii#include <stdio.h>
 #include <stdarg.h>
-#include <stdio.h>
+
+void print_all(const char * const format, ...);
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- * Return: void
+ * print_c - Prints a char argument
+ * @args: Argument list
+ */
+void print_c(va_list args)
+{
+    char c = va_arg(args, int);
+
+    printf("%c", c);
+}
+
+/**
+ * print_i - Prints an integer argument
+ * @args: Argument list
+ */
+void print_i(va_list args)
+{
+    int num = va_arg(args, int);
+
+    printf("%d", num);
+}
+
+/**
+ * print_f - Prints a float argument
+ * @args: Argument list
+ */
+void print_f(va_list args)
+{
+    float f = va_arg(args, double);
+
+    printf("%f", f);
+}
+
+/**
+ * print_s - Prints a string argument
+ * @args: Argument list
+ */
+void print_s(va_list args)
+{
+    char *str = va_arg(args, char *);
+
+    if (str == NULL)
+        printf("(nil)");
+    else
+        printf("%s", str);
+}
+
+/**
+ * print_all - Prints arguments based on format string
+ * @format: Format string containing types of arguments
+ * @...: Variable number of arguments to print
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str, *sep = "";
+    va_list args;
+    char *separator = "";
+    int i = 0, j;
 
-	va_start(args, format);
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%s%c", sep, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s%s", sep, str);
-				break;
-			default:
-				i++;
-				continue;
-		}
-		sep = ", ";
-		i++;
-	}
-	printf("\n");
-	va_end(args);
+    /* Array of structs to map format characters to print functions */
+    format_map mapping[] = {
+        {'c', print_c},
+        {'i', print_i},
+        {'f', print_f},
+        {'s', print_s},
+        {'\0', NULL}
+    };
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        j = 0;
+
+        while (mapping[j].format)
+        {
+            if (format[i] == mapping[j].format)
+            {
+                printf("%s", separator);
+                mapping[j].func(args);
+                separator = ", ";
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    printf("\n");
+    va_end(args);
 }
+
+int main(void)
+{
+    print_all("cifs", 'H', 42, 3.14, "Hello");
+    return (0);
+}
+
